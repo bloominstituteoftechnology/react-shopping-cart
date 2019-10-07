1503cb7f4927462a08d2a5dca980aa24ae6a9b57
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-07 13:15:57 +0100

    Remove commented out bits

--- src/App.js
+++ src/App.js
@@ -22,26 +22,7 @@ function App() {
 			<ProductContext.Provider value={{products, addItem}}>
 				<CartContext.Provider value={{cart}}>
 					<Navigation cart={cart} />
-
-					{/* Routes */}
-					{/* <Route
-						exact
-						path="/"
-						render={() => (
-							<Products
-								products={products}
-								addItem={addItem}
-							/>
-						)}
-					/> */}
-
 					<Route exact path='/' component={Products} />
-
-					{/* <Route 
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
-					/> */}
-
 					<Route path='/cart' component={ShoppingCart} />
 				</CartContext.Provider>
 			</ProductContext.Provider>
