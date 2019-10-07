498c3c7019c69b22376ed4bf397bf0a45804e2b8
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-07 12:56:01 +0100

    Remove references to props

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,7 +4,7 @@ import { ProductContext } from '../contexts/ProductContext';
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products = () => {
 	const { products, addItem } = useContext(ProductContext);
 	return (
 		<div className="products-container">
@@ -12,7 +12,7 @@ const Products = props => {
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
