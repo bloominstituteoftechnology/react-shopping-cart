4fbb922207cbe6c3642ee019af92ef92b295558d
Wais <95wais@gmail.com>
2019-11-09 23:25:05 -0500

    made it work, working on deleting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import {ProductContext} from './context/ProductContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-
+import {CartContext} from './context/CartContext';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{products, addItem}}>
+		<CartContext.Provider value={{cart, addItem}}>
+		<Navigation cart={cart} />
+			
 
-			{/* Routes */}
+			
 			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				exact path='/' component={Products}
 			/>
 
 			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				path='/cart' component={ShoppingCart}
 			/>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
---
+++ src/context/ProductContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const ProductContext = createContext();
+console.log(ProductContext);
+
+ 
