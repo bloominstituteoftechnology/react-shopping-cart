62540695a67ddb81eb2a09342896f574367ebfee
Cory Murray <corymurray@Corys-MacBook-Pro.local>
2019-11-12 21:09:11 -0700

    early

--- src/App.js
+++ src/App.js
@@ -7,34 +7,50 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Context
+import ProductContext from './context/ProductContext';
+import CartContext from './context/CartContext';
+
+//Hooks
+import useLocalStorage from './hooks/useLocalStorage';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		cart.push(item);
+		setCart(cart);
+		console.log(cart);
+	};
+
+	const removeItem = item => {
+
+		cart.splice(cart.indexOf(item), 1)
+		setCart(cart);
+		console.log(cart);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+			<ProductContext.Provider value = {{ products, addItem }}>
+				<CartContext.Provider value = {{cart, removeItem}}>
+					<Navigation/>
+					<Route
+						exact
+						path="/"
+						component = {Products}
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					<Route
+						path="/cart"
+						component = {ShoppingCart}
+						/>
+				</CartContext.Provider>
+			</ProductContext.Provider>
+
 		</div>
 	);
 }
