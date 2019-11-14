1f203f600ec9cb073af3d93c78494fed8353a608
Toniddarden1 <Toniddarden@gmail.com>
2019-11-13 22:15:38 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
 // Contexts
-import {ProductContext} from "./contexts/ProductContext";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -21,17 +22,15 @@ function App() {
 
   return (
     <div className="App">
-      <ProductContext.Provider value={{products, addItem}}>
-        <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{cart, setCart}}>
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route
-          exact
-          path="/"
-          render={() => <Products products={products} addItem={addItem} />}
-        />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
 
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useContext } from "react";
 
 // Components
-import Item from './ShoppingCartItem';
+import Item from "./ShoppingCartItem";
+import { CartContext } from "../contexts/CartContext";
 
-const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
+const ShoppingCart = () => {
+  const { cart } = useContext(CartContext);
+  const getCartTotal = () => {
+    return cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
 
-	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+  return (
+    <div className="shopping-cart">
+      {cart.map(item => {
+        return (
+          <div>
+            <Item key={item.id} {...item} />
+          </div>
+        );
+      })}
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
-	);
+      <div className="shopping-cart__checkout">
+        <p>Total: ${getCartTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCart;
