e93ef17cc93eee58c8d63a45ac8461826c7e9c3b
Ashley Dunham <xenocide21@gmail.com>
2019-12-09 10:56:06 -0800

    created contexts and updated components and app, this should be MVP. short and simple (i just did what we did in the lecture, no stretch goals attempted today.)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
-// Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+import { CartContext } from './contexts/CartContext.js';
+import { ProductContext } from './contexts/ProductContext.js';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+		<ProductContext.Provider value = {{ products, addItem }}>
+			<CartContext.Provider value ={{ cart }}>
+				<div className="App">
+					<Navigation cart={cart} />
+					<Route
+						exact
+						path="/"
+						component={Products}
 					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
