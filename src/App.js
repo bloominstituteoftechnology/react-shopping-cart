7b9cac44f6bc36ff12c8288694d300bc96a08631
CLYPR Technologies <cbanks@clypr.co>
2019-12-18 22:49:47 -0500

    completed steps 6 and 7, MVP complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from "./context/ProductContext";
+import CartContext from "./context/CartContext";
 
 function App() {
 	const [products] = useState(data);
@@ -18,16 +19,18 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Prover value={ cart }>
 
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation />
 
 			{/* Routes */}
 			<Route exact path="/" component={Products}/>
 
 			<Route path="/cart" component={ShoppingCart}/>
-			
+
 		</div>
+		</CartContext.Prover>
 		</ProductContext.Provider>
 	);
 }
