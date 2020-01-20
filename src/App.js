3431b3bc638c34f688e1719e746435118a86d67c
Nicole white <you@example.com>
2020-01-20 08:52:11 -1000

    imported context in app, wrapped components, updated on product route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Contexts
+import ProductContext from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -18,24 +21,19 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}/>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				exact path="/"
+				component = {Products}/>		
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			<ProductContext.Provider />
 		</div>
 	);
 }
