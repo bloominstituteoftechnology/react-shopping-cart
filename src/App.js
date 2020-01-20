267eb24441cb4997f9a0ea4111ee5a016c019ce8
Amber Mohler <ambermohler@iMac.local>
2020-01-20 14:07:04 -0500

    completed step 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 //contexts
 import {ProductContext} from './contexts/ProductContext'
+import {CartContext} from './contexts/CartContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -21,24 +22,21 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					render={() => (
-						<Products
-							products={products}
-							addItem={addItem}
-						/>
-					)}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					<Route
+						path="/cart"
+						component={ShoppingCart} />}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import CartContext from '../contexts/CartContext'
 
 // Components
 import Item from './ShoppingCartItem';
