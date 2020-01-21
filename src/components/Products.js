9bd7416b957ae2df5b4c7e98b982c3d8922e7ff2
Adam <adammonast@Adams-MBP.attlocal.net>
2020-01-21 07:50:10 -0500

    submitting MVP project

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+// Contexts
+import {ProductContext} from '../contexts/ProductContext';
+
+function Products()  {
+	const { products, addItem } = useContext(ProductContext);
+	// console.log("products: ", products);
+	// console.log("add item: ", addItem);
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
