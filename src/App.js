f742038d1a0226a886fa575718bdffec756ec9ef
Eian Carter <eian@bricleir.com>
2020-01-21 06:59:02 -0800

    refactored ShoppingCart, imported context and removed props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -18,19 +19,21 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+				<div className="App">
+					<Navigation />
 
-				{/* Routes */}
-				<Route 
-					exact path="/" 
-					component={Products}
-				/> 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					{/* Routes */}
+					<Route 
+						exact path="/" 
+						component={Products}
+					/> 
+					<Route
+						path="/cart"
+						component={ShoppingCart} 
+					/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
