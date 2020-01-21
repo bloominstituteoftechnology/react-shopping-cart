537d5d921795bd53ac4fac02a3bb591fecf8f39f
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-01-21 17:05:58 -0500

    finished project

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import CartContext from "../context/CartContext";
+import { NavLink } from "react-router-dom";
 
-const Navigation = props => {
-	return (
-		<div className="navigation">
-			<NavLink to="/">Products</NavLink>
-			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
-			</NavLink>
-		</div>
-	);
+const Navigation = () => {
+  const { cart } = useContext(CartContext);
+  return (
+    <div className="navigation">
+      <NavLink to="/">Products</NavLink>
+      <NavLink to="/cart">
+        Cart <span>{cart}</span>
+      </NavLink>
+    </div>
+  );
 };
 
 export default Navigation;
