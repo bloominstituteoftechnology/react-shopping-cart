d28a6515c81d86ae20d762dbb8f101dab38556ab
Jessica Wolff <jleahwolff@gmail.com>
2020-01-21 22:36:04 -0600

    Finished.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
-import ItemContext from '../src/context/ItemContext';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Function
 function App() {
@@ -17,29 +18,27 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
-
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<CartContext.Provider value={{cart}}>
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+				<Navigation cart={cart} />
+
+				{/* Routes */}
+				<Route
+					exact path='/' component={Products}
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				<Route
+					path="/cart"
+					component={ShoppingCart}
+					/>
+					</CartContext.Provider>
+		</ProductContext.Provider>
 		</div>
 	);
 }
