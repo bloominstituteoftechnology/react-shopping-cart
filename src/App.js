09a084e80759d597dd15c9bd66d40a64bbd98eaf
Wangdi Tenzing <wangdi457@gmail.com>
2020-01-29 16:20:23 -0500

    created Product context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from "./contexts/ProductContext;
 
 function App() {
 	const [products] = useState(data);
