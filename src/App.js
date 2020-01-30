9c565f29c60a550d72d6adb1315dc34e328c71db
ConfusionOrb221 <nate1898@gmail.com>
2020-01-30 00:04:20 -0500

    finished mvp and stretch

--- src/App.js
+++ src/App.js
@@ -6,35 +6,36 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([
+				...cart,
+				item
+			])
 	};
+
+	const removeItem = id => {
+		setCart(cart.filter((obj) => obj.id != id));
+	}
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value ={{products, addItem, removeItem}} >
+			<CartContext.Provider value ={cart} >
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route exact path="/" component={Products}/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				<Route path="/cart" component={ShoppingCart} />
+			</CartContext.Provider>
+		</ProductContext.Provider>
 		</div>
 	);
 }
