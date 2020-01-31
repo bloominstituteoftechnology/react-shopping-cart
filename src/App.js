a51b068808ab91160dada2ba5b7914c38e2567ea
Kayla Dailey <kayladailey000@gmail.com>
2020-01-31 09:15:25 -0600

    completed steps 5-6

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //Context
 import {ProductContext} from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext value = {{cart}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -40,9 +42,9 @@ function App() {
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			   component = {ShoppingCart} />
 		</div>
+		</CartContext>
 		</ProductContext.Provider>
 	);
 }
