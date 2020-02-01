a1135698b4ed0a75d7180f8a7fb0f824f38d2953
Veto Ramirez <vetorobert3@gmail.com>
2020-02-01 09:30:42 -0800

    Fix bug. Had cart in an array in the Navigation component.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { CartContext } from '../contexts/CartContext';
 
 const Navigation = () => {
-  const [ cart ] = useContext(CartContext);
+  const cart = useContext(CartContext);
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
