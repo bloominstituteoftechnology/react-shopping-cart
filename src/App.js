d30919db0687c1f029779e36bd6f62306ee01774
LittleBoGeek <NadiaChilds@outlook.com>
2020-01-31 21:38:17 -0500

    MVP met

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import  { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -15,28 +18,30 @@ function App() {
 		setCart([...cart, item]);
 	};
 
+	const removeItem = itemId => {
+		setCart(cart.filter( cartItem => cartItem.id !== itemId));
+	}
+	
+
+
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{products, addItem}}> 
+		<CartContext.Provider value={{cart, removeItem}}>
+
+			<Navigation />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<Route exact path="/" component={Products} />
+			
+			<Route path="/cart" component={ShoppingCart}/>
+			
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
 
 export default App;
+
+
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react'; // import createContext using curly braces because it's nota default export
+
+
+export const CartContext = createContext(); // set a constant, the variable name of our context  = to createContext.
+
+
