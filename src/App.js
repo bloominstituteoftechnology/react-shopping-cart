757677a0b1d9c08c14fe4f52ff24295ec719ec6a
shiva banihashem <sbanihas@gmail.com>
2020-04-16 21:21:55 -0500

    MVP and removeItem stretch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+// Context
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -13,21 +16,26 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
-
+	const removeItem = id => {
+		setCart(cart.filter(item => {
+			return item.id !== id;
+		}))
+	}
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }} >
+			<CartContext.Provider value={{ cart, removeItem }} >
+				{/* Routes */}
+				<div className="App">
+					<Navigation cart={cart} />							
+					<Route exact path="/" component={Products} />
+					<Route exact path="/cart" component= {ShoppingCart}/>
+					
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
