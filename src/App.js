670090a2a9eb05a2ff09b8f9bb09ba8d5dbf2649
Andrew Sitzes <andrewsitzes@Andrews-MacBook-Pro.local>
2020-05-14 16:07:38 -0700

    itsthemvp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {Context} from './context/context';
+import { CartContext } from './context/cartContext';
 
-// Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
@@ -10,23 +11,23 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
-
 	const addItem = item => {
-		// add the given item to the cart
+	setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+		<Context.Provider value={{products, addItem}}>
+		<CartContext.Provider value ={ cart }>
+		<Navigation cart={cart} />
+		<Route exact path="/">
+		<Products />
+		</Route>
+		<Route path="/cart">
+		<ShoppingCart />
+		</Route>
+		</CartContext.Provider>
+		</Context.Provider>
 		</div>
 	);
 }
