e4204ee0abecb411fc94f86b9d66fa9d227db849
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-11 18:08:43 -0500

    MVP

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
-import ProductContext from "../contexts/ProductContext";
+import { ProductContext } from "../contexts/ProductContext";
 
 // Components
 import Product from "./Product";
 
 const Products = () => {
   const { products, addItem } = useContext(ProductContext);
-
   return (
     <div className='products-container'>
       {products.map((product) => (
