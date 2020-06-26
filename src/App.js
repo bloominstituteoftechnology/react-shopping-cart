135aec451f4a23e8cf2a8edef5107d1aa4088271
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-06-25 18:05:11 -0700

    start

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
-// Components
+// Components//
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
