e3831392ef9a28b8a0fb0d49c3b997c5259e7e8d
JenVest2020 <jenvest2019@gmail.com>
2020-07-29 18:56:26 -0500

    having map issues in shoppingcart component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -22,18 +23,22 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+				<Navigation />
+			</CartContext.Provider>
 
 			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
 				<Route exact path="/">
 					<Products />
 				</Route>
-
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
 			</ProductContext.Provider>
+
+			<CartContext.Provider value={{ cart }}>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
