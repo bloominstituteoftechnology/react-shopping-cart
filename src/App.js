3c1b95b7a78a429e542153b5bccbb3a6bf255990
royeradames <royeraadames@gmail.com>
2020-08-13 15:36:00 -0400

    provided data with productcontext

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//contenxt
+import ProductContext from './contexts/ProductContext'
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -19,15 +22,18 @@ function App() {
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</ProductContext.Provider>
+
 		</div>
 	);
 }
