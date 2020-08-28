b9c34bdd195d33415202d7481c7ab2c65dd6e1f5
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-28 14:11:53 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+
 
 function App() {
 	const [products] = useState(data);
@@ -13,20 +17,30 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
+		
+	};
+
+	const removeItem = id => {
+		setCart(cart.filter(item=> item.id !== id));
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+	<ProductContext.Provider value={{products, addItem}}>
+		<CartContext.Provider value={{cart, removeItem}}>
+			<Navigation />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+		</CartContext.Provider>
+	</ProductContext.Provider>
 		</div>
 	);
 }
