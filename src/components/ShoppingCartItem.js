2135798500423147a8c7e3363710cb6f4a4fd0be
River Smith <rivyyriv@rivers-mbp.lan>
2020-09-02 14:33:14 -0600

    MVP

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Item = props => {
+	console.log(props)
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +11,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={() => props.removeItem(props.id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
