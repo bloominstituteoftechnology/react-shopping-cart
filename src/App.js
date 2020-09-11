babc08fc712f98a0f9d9d92b7bf1be4b03423f79
unknown <olem2233@gmail.com>
2020-09-10 19:59:08 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,20 +16,32 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
+	};
+
+	const removeItem = id => {
+		setCart(cart.filter(item=> item.id !== id));
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{cart, removeItem}}>
+
+			<Navigation />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+
+			</CartContext.Provider>
+			</ProductContext.Provider>
+
 		</div>
 	);
 }
