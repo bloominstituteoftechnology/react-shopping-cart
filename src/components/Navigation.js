7c1dbe87681d0dd0861b119a49420a5832c55f30
Ryan Clark <rbclark244@gmail.com>
2020-09-27 18:26:47 -0400

    can't get cart to work

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
 
-const Navigation = props => {
-	return (
-		<div className="navigation">
-			<NavLink to="/">Products</NavLink>
-			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
-			</NavLink>
-		</div>
-	);
+const Navigation = () => {
+  const { cart } = useContext(CartContext);
+  return (
+    <div className="navigation">
+      <NavLink to="/">Products</NavLink>
+      <NavLink to="/cart">
+        Cart <span>{cart.length}</span>
+      </NavLink>
+    </div>
+  );
 };
 
 export default Navigation;
