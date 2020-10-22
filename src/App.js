72b7d6e99010e0fc0d9835b2f14bb519432c7d1d
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-22 17:46:14 -0600

    step 1, completed addItem function

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([ ...cart, item])
+		console.log(cart)
 	};
 
 	return (
