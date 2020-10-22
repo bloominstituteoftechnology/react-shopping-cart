9bad4b82bc6148652a1e965d761fd8512d37c9f6
Mychael M <mychaelbmenges@gmail.com>
2020-10-22 15:27:59 -0600

    added context to consume data from context api in products component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import { ProductContext } from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -21,19 +24,21 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+    <div className="App">
+      <ProductContext.Provider value={{products, addItem}}>
+        <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+        {/* Routes */}
+        <Route exact path="/">
+          <Products />
+        </Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+        <Route path="/cart">
+          <ShoppingCart cart={cart} />
+        </Route>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
