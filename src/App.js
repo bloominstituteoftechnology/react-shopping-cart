0ca49ec2d0e6566a7a82b905daea6e406af1fea8
LBU <lbu@LBUui-MacBook-Pro.local>
2020-11-05 17:07:39 -0500

    last commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext'
-import { CartContext } from './contexts/CartContext'
+import  { CartContext }  from './contexts/CartContext'
 
 // Components
 import Navigation from './components/Navigation';
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,3 +1,4 @@
 import React, { createContext } from 'react'
 
+
 export const ProductContext = createContext()
