0afc52765543f8f6a81e91c0902a2e0066bec54c
LBU <lbu@LBUui-MacBook-Pro.local>
2020-11-05 15:58:26 -0500

    first commit, mvp met till step4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,21 +14,24 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		return cart.push(item)
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provder value={{ products, addItem }}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products/>
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</div>
+		</ProductContext.Provder>
 	);
 }
 
---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,3 @@
+import { useContext, createContext} from 'react'
+
+export const ProductContext = createContext()
