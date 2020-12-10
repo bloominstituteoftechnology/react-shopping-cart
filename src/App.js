967c01f7acd4b90c600018829c457dd918f375a2
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2020-12-10 16:59:31 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //contexts
 import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,19 +22,21 @@ function App() {
 	};
 
 	return (
-		<div className="App">
+		<div className="App">			
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value ={{ cart }}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
+					<Route path="/cart">
+						<ShoppingCart />
 
-				</Route>
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
