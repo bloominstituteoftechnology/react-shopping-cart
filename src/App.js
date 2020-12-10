89f700dfa78e5bc0fb7e520206c38b27878955fa
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2020-12-10 18:27:29 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,25 +7,50 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+export const CartContext = createContext();
+export const ProductContext = createContext();
+
 function App() {
+
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
-	};
+			 setCart([
+				...cart,
+				item
+			 ])	
+	} 
+	
+	const removeItem = item => {
+		const oldItem = products.filter((item) => {
+			return item === item
+		})
+		setCart([
+			...cart,
+			oldItem
+		])
+	}
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<CartContext.Provider value={cart}>
+			<Navigation/>
+		</CartContext.Provider>
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<Products  />
+			</ProductContext.Provider>
+				
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+			<CartContext.Provider value={{cart, removeItem}}>
+				<ShoppingCart />
+			</CartContext.Provider>
+				
 			</Route>
 		</div>
 	);
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ProductContext} from './../App';
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products = ( props ) => {
+	
+	const { products, addItem } = useContext(ProductContext)
+	
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
