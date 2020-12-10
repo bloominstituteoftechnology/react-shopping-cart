3470be41e0f00a628ffe891cf7b881f40f91c8e3
CerritoCode0101 <CaseyCerrito@gmail.com>
2020-12-10 11:44:11 -0600

    mvp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-const Navigation = props => {
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { CartContext } from '../contexts/CartContext';
+const Navigation = () => {
+	const { cart } = useContext(CartContext);
+	console.log(cart)
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+			     Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
