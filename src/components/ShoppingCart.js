683f623a93830d530369509227644472161ad98d
cmirza <mail@cmirza.com>
2020-12-10 17:30:06 -0800

    MVP and removeItem functionality

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Item from './ShoppingCartItem';
+// Contexts
+import CartContext from '../contexts/CartContext';
 
 const ShoppingCart = props => {
+
+	const { cart } = useContext(CartContext);
+
 	const getCartTotal = () => {
 		return props.cart.reduce((acc, value) => {
 			return acc + value.price;
@@ -12,7 +17,7 @@ const ShoppingCart = props => {
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
