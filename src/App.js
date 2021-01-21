967eb40d7aa2499a3cb4e853c2f4f7ac23d0e620
DavidHall-Code <david_m_hall@outlook.com>
2021-01-21 18:00:55 -0500

    Setup addItem

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
-	const addItem = item => {
+	const addItem = (item) => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
