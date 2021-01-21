5e918d2970c70fae6d329b83d12b8e6c9e069ea4
Diego Roman <diegormnv209@gmail.com>
2021-01-21 14:24:24 -0800

    Finished addItem Logic

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
-
+	
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
