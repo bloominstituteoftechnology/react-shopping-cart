a626970152eed3abdef7ebaa11ccf87544408a0d
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-01-21 20:57:48 -0700

    MVP

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
 
-// Components
 import Item from './ShoppingCartItem';
 
 const ShoppingCart = () => {
