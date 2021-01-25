16b6bfa51820b5ff27e044a0f2be3838df9757c8
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-01-25 07:25:49 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,27 +6,31 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import Context from "./contexts/context"
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart(item)
 		// add the given item to the cart
 	};
 
 	return (
 		<div className="App">
+			<Context.themeProvider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</Context.themeProvider>
 		</div>
 	);
 }
