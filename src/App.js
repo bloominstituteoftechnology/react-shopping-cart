21c97c16aa674e4395abfea8cad626cf6b97d013
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-02-04 23:14:27 -0800

    Initial setup and some work done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from "./contexts/productContext";
+import {cartContext} from './contexts/cartContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -11,22 +13,32 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
-	const addItem = item => {
+	const addItem = (item) => {
 		// add the given item to the cart
+		setCart([...cart, item]);
+	};
+	const removeItem = (itemId) => {
+		setCart(cart.filter((item) => item.id !== itemId));
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<cartContext.Provider value={{ cart, removeItem}}>
+				<Navigation/>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+
+				</cartContext.Provider>	
+			</ProductContext.Provider>
+			
 		</div>
 	);
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+import { cartContext} from '../contexts/cartContext';
 
-const Navigation = props => {
+const Navigation = () => {
+	const { cart } = useContext(cartContext);
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+				Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
