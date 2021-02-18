69dd83d746cf3593926241c8cb34c8d9e4035756
CaseyBlatz <caseyblatz@yahoo.com>
2021-02-18 16:19:00 -0700

    MVP

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import ProductContext from '../context/ProductContext';
 
 const Product = props => {
+	const {removeItem} = useContext(ProductContext)
+	const remove = () => {
+		removeItem()
+	}
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
@@ -12,6 +17,7 @@ const Product = props => {
 			<button onClick={() => props.addItem(props.product)}>
 				Add to cart
 			</button>
+			<button onClick={remove}>Remove Item</button>
 		</div>
 	);
 };
