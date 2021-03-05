8c6c3a2af97a85fa5391e5f47980a2b3da060c4c
daniel-vazquez <da.vazquez728@gmail.com>
2021-03-04 17:01:43 -0700

    implemented useContext in products.js component and refactored from using props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,20 +14,22 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
-
+			<ProductContext.Provider value={products, addItem}>
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
