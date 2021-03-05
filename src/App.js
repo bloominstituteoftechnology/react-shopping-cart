417d388ea4a2f574e97740ee82b55525f0e32a84
Durrell Jules <dujules23@gmail.com>
2021-03-04 21:17:33 -0500

    Correct issues with addItem function, fixed CartContext, project complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+//imported contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext'
+
 
 // Components
 import Navigation from './components/Navigation';
@@ -15,24 +18,38 @@ function App() {
 	const addItem = item => {
 		// add the given item to the cart
 		
-		setCart(item)
-		console.log(item)
+		// products.filter((item, i) => {
+		// 	if(products[i] === item[i]){
+		// 		return setCart(item)
+		// 	}
+		// 	else{
+		// 		return cart
+		// 	}
 		
-	};
+		// })
 
+		//setCart to cart (must be an array if using spread operator)
+		setCart([...cart,item])
+	};
+	console.log(cart)
+	
+	//Values only accept an object 
+	
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{ products, addItem} }>
-				<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem, cart} }>
+				<CartContext.Provider value={{cart}}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products  />
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products  />
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path="/cart">
+						<ShoppingCart  />
+					</Route>
+				</CartContext.Provider>	
 			</ProductContext.Provider>	
 		</div>
 	);
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react'
+
+//Step 1 : create context (same as creating store in redux)
+export const CartContext = createContext();
